a10d5efae24d64b80a89f3a60b107b680c066d79
Barbara Machado <147945840+BarbarizandoDev@users.noreply.github.com>
2023-11-19 20:43:09 +0000

    atualizado em 19-11-23

---
+++ src/CadastroPacientes/CadastramentoPacientes.java
@@ -0,0 +1,16 @@
+
+package CadastroPacientes;
+
+/**
+ *
+ * @author barba
+ */
+public class CadastramentoPacientes {
+
+    
+    public static void main(String[] args) {
+       TelaInicial tela = new TelaInicial ();
+        tela.setVisible(true);
+    }
+    
+}
